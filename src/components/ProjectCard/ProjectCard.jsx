1038bb1845b14226acb9aff6a4fa8e5296734cf0
Eldyar <thedolbekov2@gmail.com>
2024-01-16 22:07:08 +0300

    Добавил форму заявки

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
-import {styles} from './ProjectCard.module.css'
+// import React from 'react'
+import styles from './ProjectCard.module.scss'
 
-const ProjectCard = () => {
+const ProjectCard = (project) => {
   return (
-    <div>ProjectCard</div>
+    <a href="#" onClick={(e) => e.preventDefault()}>
+      <img 
+        src={project.projectScreen !== '' ? project.projectScreen : project.defaultProjectScreen} 
+        alt="project img" 
+        className={styles.projectCard__img} 
+      />
+      <div className={styles.projectCard__name}>{project.title}</div>
+      <p className={styles.projectCard__price}>{project.price}</p>
+    </a>
   )
 }
 
